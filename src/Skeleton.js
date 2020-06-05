10e130e81c07c4f0437125c2af8d183c4f48f075
shreyas <shreyas@gmetri.com>
2020-06-05 14:22:36 +0530

    Adding a skelton for the first render for SSR

---
+++ src/Skeleton.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <div className="table_container">
+      {/* simple heading */}
+      <div className="text-center heading-1 primary bold mt-16 mb-16">
+        Hacker News
+      </div>
+
+      <div> Loading ... </div>
+    
+    </div>
+    )
+}
