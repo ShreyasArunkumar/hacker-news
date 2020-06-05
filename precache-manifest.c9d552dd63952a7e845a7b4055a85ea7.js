66f3b3dc3d24c626e3bb30943430aaab01a3b48b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-05 09:13:12 +0000

    Deploy ShreyasArunkumar/hacker-news to github.com/ShreyasArunkumar/hacker-news.git:gh-pages

---
+++ precache-manifest.c9d552dd63952a7e845a7b4055a85ea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "968172292d36238f0c2eec03fd61f27b",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "c53665b9009d648e3334",
+    "url": "/hacker-news/static/css/main.3303df5b.chunk.css"
+  },
+  {
+    "revision": "68072f6956c8dda09d02",
+    "url": "/hacker-news/static/js/2.f4184cda.chunk.js"
+  },
+  {
+    "revision": "583ce86d7f120f4227d5f98b4c47211e",
+    "url": "/hacker-news/static/js/2.f4184cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53665b9009d648e3334",
+    "url": "/hacker-news/static/js/main.61d5d737.chunk.js"
+  },
+  {
+    "revision": "3058bd9c9b16bc5cbe6d",
+    "url": "/hacker-news/static/js/runtime-main.b66f509d.js"
+  }
+]);
